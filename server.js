833e20378b963443af593f50b73492b8577f7852
Harry Patrick <harry.g.patirck94@gmail.com>
2020-02-26 11:03:15 +1100

    added allor cors everywhere

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ const auth = require('./auth')
 const db = process.env.MONGODB_URL || config.get('mongoURI');
 // const db = config.get('mongoURI')
 //mongoose connect
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 mongoose
   .connect(db, {
         useNewUrlParser: true,
